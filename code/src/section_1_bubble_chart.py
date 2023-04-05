9268c336c80959a12064077c63c51eff1874ed1f
PatriciaYep <129228053+PatriciaYep@users.noreply.github.com>
2023-04-04 21:28:46 -0400

    Update section_1_bubble_chart.py

--- code/src/section_1_bubble_chart.py
+++ code/src/section_1_bubble_chart.py
@@ -8,13 +8,41 @@ from template import THEME
 
 def get_figure(data):
     '''
-        !!!!!!!!!!!!Add description here!!!!!!!!!
+        Generates the bubble plot.
+        
+        Day in axe X, patient in axe Y, 
+        number of visits in marker size
+        and pain indicator in marker color
 
-        Args: !!!!!!add arguments!!!!!!!!!!!
+        Args: 
+            data: The dataframe to display
             
         Returns: 
             The figure to be displayed
         
     '''
 
-    return None
+    fig = px.scatter(my_df, y='PATIENT_ID', x='DAY', 
+                     color='HAS_PAIN_MENTION', size='VISIT_COUNTS', 
+                     size_max=10, width=1200, height=700,
+                     title='Douleur chez les patients vs nombre de visites',
+                     color_discrete_sequence=['#7786FA','#FF6100'],
+                     labels=dict(PATIENT_ID='<b>Nom du Patient</b>', DAY='<b>Jour</b>', HAS_PAIN_MENTION='Douleur')
+                     
+                     )
+    
+    fig.update_layout(
+                    font_color='#848D99',
+                    title_font_size=40,
+                    title_x=0.5 #center title
+    )
+    
+    fig.update_xaxes(tickangle = -45,
+                     nticks=28
+                     )
+    
+    fig.update_traces(
+                     hovertemplate=hover_template.get_bubble_hover_template()
+                     )
+
+    return fig
