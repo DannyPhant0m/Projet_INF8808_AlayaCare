6791136fb945cf85a0c1f9ce54ccebcb69cb0694
amanda-rodrigues-delfiol <129908591+amanda-rodrigues-delfiol@users.noreply.github.com>
2023-04-11 15:25:31 -0400

    Update section_1_heatmap.py

--- code/src/section_1_heatmap.py
+++ code/src/section_1_heatmap.py
@@ -8,13 +8,30 @@ from template import THEME
 
 def get_figure(data):
     '''
-        !!!!!!!!!!!!Add description here!!!!!!!!!
+        This function generates the heatmap that depicts the percentage of
+        activity completio for each patient over the dates from the dataframe imput
 
-        Args: !!!!!!add arguments!!!!!!!!!!!
+        Arg:
+            data - a wide type dataframe
             
         Returns: 
             The figure to be displayed
         
     '''
+    fig = px.imshow(data,
+                    labels=dict(x="Journée",
+                                y="Patient", 
+                                color="Taux de complétion d'activtiés (%)"),
+                    color_continuous_scale=['#FFE3C0','#FFDFBA','#FFA73A','#F89213','#FF6100']
+                )
+
+    fig.update_layout(title='Pourcentage de complétion des activités au long des visites',
+                      xaxis_nticks=len(data.columns),
+                      paper_bgcolor='rgba(0,0,0,0)',
+                      plot_bgcolor='rgba(0,0,0,0)'
+                    )
+    
+    fig.update_traces(hovertemplate = hover_template.get_heatmap_hover_template())
+    
 
     return None
