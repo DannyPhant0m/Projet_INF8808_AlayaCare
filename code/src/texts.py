6683cf4574334e1b7acd5d7edddc9513e1a25e69
amanda-rodrigues-delfiol <129908591+amanda-rodrigues-delfiol@users.noreply.github.com>
2023-04-14 12:47:44 -0400

    grammar and some text

--- code/src/texts.py
+++ code/src/texts.py
@@ -23,7 +23,7 @@ HOME_DESCRIPTION = " Texte à completer "
     Section 1.2:  Progression du niveau de complétion des activités par patient 
 '''
 
-
+SECTION_1_2_HEATMAP_1_DESCRIPTION = " Ce graphique a comme objectif de dresser le portrait des progressions des niveaux de complétion d'activités par patient "
 
 
 
@@ -38,7 +38,7 @@ HOME_DESCRIPTION = " Texte à completer "
     Section 2.2 : Nombre de notes et d'hospitalisations
 '''
 
-SECTION_2_GROUPED_BAR_HEADER = " Ce section présente la corrélation possible entre les notes prises sur un patient comparée au nombre de chutes et d'hospitalisation \n"
+SECTION_2_GROUPED_BAR_HEADER = " Cette section présente la corrélation possible entre les notes prises sur un patient comparée au nombre de chutes et d'hospitalisation \n"
 
 SECTION_2_GROUPED_BAR_1_DESCRIPTION = " Ce graphique à barres groupées permet de distinguer rapidement la disparité entre les notes prises et le nombre d'hospitalisations "
 
