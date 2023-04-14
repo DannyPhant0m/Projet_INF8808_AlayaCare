aba7433145d28049abdae487217f9cfc363a1f05
amanda-rodrigues-delfiol <129908591+amanda-rodrigues-delfiol@users.noreply.github.com>
2023-04-14 15:55:22 -0400

    text home and heatmap

--- code/src/texts.py
+++ code/src/texts.py
@@ -8,7 +8,20 @@
 '''
 
 
-HOME_DESCRIPTION = " Texte à completer "
+HOME_DESCRIPTION = " AlayaCare est une compagnie responsable par un logiciel de gestion des soins à domicile pour les agences du domaine. "\
+" Les visualisations de données dans ce projet ont comme but d'outiller les utilisateurs du logiciel avec des comparaisons intuitives"\
+" par rapport aux différents métriques enregistrées chez les patients. Cette vue d'ensemble a un potentiel pour identifier les"\
+" tendences et comportements des patients ainsi que pour donner un aperçu général de la qualité des soins "\
+" Les prochains onglets cherchent à répondre plusieurs questions réliées à cette thématique:"\
+" Douleur et visites - Est-ce que la douleur diminue chez les patients avec le nombre de visites?"\
+" Progression du niveau de complétion - Est-ce que le pourcentage de complétion d’activités de la vie quotidienne d’un patient augmente avec le nombre de visites durant les derniers 28 jours?"\
+" Notes et hospitalisations - Est-ce que le nombre de notes prises totale par patient est proportionnel au nombre d' hospitalisations?"\
+" et est-ce que le nombre de notes prises totale par patient est proportionnel au nombre de chutes?"\
+" Chutes et hospitalisations - Est-ce que le nombre de chutes d’un patient augmente ou diminue au cours de ses visites durant les derniers 28 jours?"\
+" et est-ce qu’il existe une corrélation entre les journées de chutes et d’hospitalisation des patients durant les derniers 28 jours?"\
+" Annulation de visites - Est-ce qu'il y a une corrélation de tendance entre les patients qui annulent des rendez-vous versus le niveau de douleur ressenti au dernier rendez-vous?"\
+" et est-ce qu'il y a une corrélation de tendance entre les patients qui annulent des rendez-vous versus le pourcentage de complétions d'activités quotidiennes?"\
+" Chaque visualisation est outillée d'info-bulles pour détailler les données présentés. Il suffit de passer la souris par-dessous les éléments des figures. "
 
 
 '''
@@ -23,7 +36,9 @@ HOME_DESCRIPTION = " Texte à completer "
     Section 1.2:  Progression du niveau de complétion des activités par patient 
 '''
 
-SECTION_1_2_HEATMAP_1_DESCRIPTION = " Ce graphique a comme objectif de dresser le portrait des progressions des niveaux de complétion d'activités par patient "
+SECTION_1_2_HEATMAP_1_DESCRIPTION = " Ce graphique a comme objectif de dresser le portrait"\
+"des progressions des niveaux de complétion d'activités par patient, "\
+"ce qui permet de faire le suivi visuel du progrès des patients par rapport aux activités complétés. "
 
 
 
